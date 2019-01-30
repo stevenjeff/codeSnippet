5952e4b30de152c7a4769bc1aae809af0dc3602e
Author <stevenjeff@126.com>
2019-01-30 14:21:29 +0800

    init

---
+++ CodeSnippet/src/org/fangrui/Test.java
@@ -0,0 +1,27 @@
+package org.fangrui;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+//		System.out.println(stackTraceElements);
+//		new Exception().printStackTrace();
+		b();
+
+
+	}
+	
+	public static void a() {
+//		StackWalker stack = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
+//		stack.forEach(System.out::println);
+		java.lang.StackWalker
+		 .getInstance(java.lang.StackWalker.Option.RETAIN_CLASS_REFERENCE)
+		 .walk(s -> s.skip(1).limit(1).collect(Collectors.toList()))
+		 .forEach(System.out::println);//第一次运行，注释掉
+	}
+
+	public static void b() {
+		a();
+	}
+}
