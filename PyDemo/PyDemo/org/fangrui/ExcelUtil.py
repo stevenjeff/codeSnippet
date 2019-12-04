ae5e75f4cc204528b9f584d01290ac55da374071
zhangfangrui <416271557@qq.com>
2019-12-04 14:27:28 +0800

    通过excel 产生 json

--- PyDemo/PyDemo/org/fangrui/ExcelUtil.py
+++ PyDemo/PyDemo/org/fangrui/ExcelUtil.py
@@ -1,9 +1,14 @@
 import xlrd
 
-file_path = r'C:\Users\zhangfangrui\Desktop\需求文档\酒店\TEAM-A及测试座位图.xlsx'
+file_path = r'.\test.xlsx'
 workbook = xlrd.open_workbook(file_path)  # 打开excel工作簿
 sheet = workbook.sheet_by_index(0)  # 选择第一张sheet
 for row in range(sheet.nrows):  # 第一个for循环遍历所有行
     print()
-    for col in range(sheet.ncols):  # 第二个for循环遍历所有列，这样就找到某一个xy对应的元素，就可以打印出来
-        print("%7s" % sheet.row(row)[col].value, '\t', end='')
+    # for col in range(sheet.ncols):  # 第二个for循环遍历所有列，这样就找到某一个xy对应的元素，就可以打印出来
+    #     print("%7s" % sheet.row(row)[col].value, '\t', end='')
+    print('"%7s"' % sheet.row(row)[0].value, '', end=':')
+    if sheet.row(row)[1].value == "string":
+        print('""', end="")
+    print('', end=",")
+
