c11c5f07349753c9a37b84df5537fce6fdbd27b7
zhangfr8@sunac.com.cn <zhangfr8@sunac.com.cn>
2022-07-20 10:00:53 +0800

    powershell

---
+++ Powershell/Custom.ps1
@@ -0,0 +1,15 @@
+﻿$property = @{
+    name = 'totalSpaceGB'
+    expression = { ($_.used + $_.free) / 1GB }
+}
+$propertyFree = @{
+    name = 'totalSpaceGBFree'
+    expression = { ($_.free) / 1GB }
+}
+$propertyUsed = @{
+    name = 'totalSpaceGBUsed'
+    expression = { ($_.used) / 1GB }
+}
+
+$drives = Get-PSDrive | Where Used
+$drives | Select-Object -Property name, $property
