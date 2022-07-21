ead46748cd87f9293f2de06d798b161a10eef8c8
zhangfr8@sunac.com.cn <zhangfr8@sunac.com.cn>
2022-07-21 09:56:58 +0800

    powershell

--- Powershell/Custom.ps1
+++ Powershell/Custom.ps1
@@ -12,4 +12,51 @@ $propertyUsed = @{
 }
 
 $drives = Get-PSDrive | Where Used
-$drives | Select-Object -Property name, $property
+$drives | Select-Object -Property name, $property, $propertyFree, $propertyUsed
+$drives | Select-Object -property name, @{n='totalSpaceGB';e={($_.used + $_.free) / 1GB}}
+
+$data = @(
+    @{name='a'}
+    @{name='c'}
+    @{name='e'}
+    @{name='f'}
+    @{name='d'}
+    @{name='b'}
+)
+
+$d = $data | Sort-Object -Property @{e={$_.name}}
+
+$CIMParams = @{
+    ClassName = 'Win32_Bios'
+    ComputerName = $ComputerName
+}
+
+if($Credential)
+{
+    $CIMParams.Credential = $Credential
+}
+
+Get-CIMInstance @CIMParams
+
+$log = @{Path = '.\logfile.log'}
+Add-Content "logging this command" @log
+
+$robo = @{R=1;W=1;MT=8}
+robocopy source destination @robo
+
+$people = @{
+    Kevin = @{
+        age  = 36
+        city = 'Austin'
+    }
+    Alex = @{
+        age  = 9
+        city = 'Austin'
+    }
+}
+
+foreach($name in $people.keys)
+{
+    $person = $people[$name]
+    '{0}, age {1}, is in {2}' -f $name, $person.age, $person.city
+}
